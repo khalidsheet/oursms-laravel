a46e6e8dab68c11c0608654a4db9b6be22146fdf
Khalid Sheet <prog.khalid.mohammad@gmail.com>
2021-06-09 12:59:53 +0300

    Enhance config/oursms.php file

--- config/oursms.php
+++ config/oursms.php
@@ -1,14 +1,52 @@
 <?php
 
 return [
+    /*
+     |-----------------------------------------------------------
+     |  Oursms Config
+     | ----------------------------------------------------------
+     |
+     | This config file contains your account credentials
+     | provided by https://oursms.app service
+     |
+     | If you don't know how to get these credentials visit
+     | oursms.app help center
+     | Article: https://help.oursms.app/ar/article/kyf-ytm-astkhdam-khdma-rsalna-aan-tryk-api-1wqs4a/
+     |
+     | ----------------------------------------------------------
+     */
 
-    /**
-     * Place you user id provided by oursms.app here
+
+
+
+
+    /*
+     |-----------------------------------------------------------
+     |  User Identifier
+     | ----------------------------------------------------------
+     |
+     | Your account userId
+     |
+     | You don't have to worry about the userId and who to write
+     | it, This library will handle this kinda stuff for you
+     |
+     | For example: Your userId may be something like "SP-00434"
+     | so you can write it either "SP-00434" or just "434"
+     |
+     | ----------------------------------------------------------
      */
     'userId' => env('OURSMS_USER_ID', ''),
 
-    /**
-     * Place the key provided by oursms.app here
+
+
+    /*
+     |-----------------------------------------------------------
+     |  Account Key
+     | ----------------------------------------------------------
+     |
+     | Your account 32 characters long key
+     |
+     | ----------------------------------------------------------
      */
     'key' => env('OURSMS_KEY', '')
 ];
